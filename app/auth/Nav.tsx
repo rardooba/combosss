e6d97421db021ef048b53b15fd0a7e26f4a5626c
rardooba <rardooba@gmail.com>
2024-03-19 22:20:07 +0100

    🎉 Let's goooo Doob

---
+++ app/auth/Nav.tsx
@@ -0,0 +1,23 @@
+
+
+import Link from "next/link"
+import { signIn } from "next-auth/react"
+import Login from "./Login"
+import Logged from "./Logged"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../../pages/api/auth/[...nextauth]"
+
+export default async function Nav() {
+ const session = await getServerSession(authOptions)
+
+  return (
+    <nav className="flex justify-between items-center py-8 ">
+      <Link href={"/"}>
+        <h1 className="font-bold text-lg">Combosss.</h1>
+      </Link>
+      <ul className="flex items-center gap-6"></ul>
+      {!session?.user && <Login />}
+      {session?.user && <Logged image={session.user?.image || ""} />}
+    </nav>
+  )
+}
